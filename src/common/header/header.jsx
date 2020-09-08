c5eada066a60ccd605d90b68952203e6eb5bb777
wuweikang <1198949718@qq.com>
2020-09-08 20:15:30 +0800

    下班

--- src/common/header/header.jsx
+++ src/common/header/header.jsx
@@ -4,22 +4,38 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 export default class DIYHeader extends Component {
-  static propTypes = {
-    title: this.propTypes.string.isRequired,
-    signUp: PropTypes.bool,
-    goBack: PropTypes.func,
-    goHome: PropTypes.func,
-    edit: PropTypes.func,
-    userInfo: PropTypes.object.isRequired
-  }
-  state = {
-    headTitle: '首页'
-  }
-
+  /**
+   * 头部
+   * 
+   * @param{ default 全部自定义,backText, 返回文字, title 标题, rightContent, 右侧内容 }
+   */
+  // static propTypes = {
+  //   title: this.propTypes.string.isRequired,
+  //   signUp: PropTypes.bool,
+  //   goBack: PropTypes.func,
+  //   goHome: PropTypes.func,
+  //   edit: PropTypes.func,
+  //   userInfo: PropTypes.object.isRequired
+  // }
   render() {
+    let backText = '返回',
+      title = '',
+      rightContent;
+      
+      for (let item in this.props){
+        if (this.props.backText && item == 'backText') {
+          backText = this.props[item]
+        } else if (this.props.title && item == 'title') {
+          title = this.props[item]
+        } else if (this.props.rightContent && item == 'rightContent') {
+          rightContent = this.props[item]
+        } else {
+
+        }
+      }
     return (
       <header className='header-container'>
-        123
+        {title}
       </header>
     )
   }
